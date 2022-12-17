39888ae4dd741a105258f19ce59c7083130bed2f
Baydock <xandercorvalan@gmail.com>
2022-12-16 23:32:13 -0600

    Made BTD6EOSBypasser embedded

---
+++ BTD6EpicGamesModCompat/Resources.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace BTD6EpicGamesModCompat {
+    internal static class Resources {
+        // The current running assembly of BTD6EpicGamesModCompat
+        private static readonly Assembly thisAssembly = Assembly.GetExecutingAssembly();
+        // The name of the current running assembly
+        private static readonly string assemblyName = thisAssembly.GetName().Name.Replace(" ", "");
+        // Gets all available resource names in the assembly
+        private static readonly string[] resourceNames = thisAssembly.GetManifestResourceNames();
+
+        // Gets a resource with the given name
+        public static byte[] GetResource(string resourceName) {
+            // Convert resource name to full
+            string fullName = $"{assemblyName}.Resources.{resourceName}";
+
+            // Checks if the resource does not exist
+            if (!resourceNames.Contains(fullName))
+                return null;
+
+            // Loads and returns resource
+            using MemoryStream resourceStream = new();
+            try {
+                thisAssembly.GetManifestResourceStream(fullName).CopyTo(resourceStream);
+                return resourceStream.ToArray();
+            } catch {
+                return null;
+            }
+        }
+    }
+}
