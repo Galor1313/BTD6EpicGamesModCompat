3d881c63c2f08ca7d9d419c4676f094814fc599b
Baydock <xandercorvalan@gmail.com>
2022-12-26 10:19:22 -0600

    Added additional onclose event

--- BTD6EpicGamesModCompat/Plugin.cs
+++ BTD6EpicGamesModCompat/Plugin.cs
@@ -19,14 +19,10 @@ namespace BTD6EpicGamesModCompat {
             // Avoid crash
             EOSSDK.Remove();
 
-            // Used as a backup quit event, sometimes one and sometimes the other is called based on how the application is forcefully closed
-            // There are still some ways that this could not end up being called.
-            // Any help on how to get this to call on the following would be appreciated:
-            // - A hard crash (eg. an uncaught exception)
-            // - Task manager force close
-            // - Selecting text in melonloader console (pausing execution), and then closing from the melonloader console
-            // - Probably some other ways I haven't tried
+            // Not fully tested to work for all close cases
             AppDomain.CurrentDomain.ProcessExit += (s, e) => EOSSDK.Restore();
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => EOSSDK.Restore();
+            
         }
 
         public override void OnApplicationQuit() => EOSSDK.Restore();
