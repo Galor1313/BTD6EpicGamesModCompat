fd04291dff2aeb80b78d9f90271b241cc719136a
Baydock <xandercorvalan@gmail.com>
2023-01-05 20:40:54 -0600

    Additional error checking and purchase disabling

--- BTD6EOSBypasser/Mod.cs
+++ BTD6EOSBypasser/Mod.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using BTD6EOSBypasser;
+﻿using BTD6EOSBypasser;
 using HarmonyLib;
 using Il2Cpp;
 using Il2CppAssets.Scripts.Unity;
 using Il2CppAssets.Scripts.Unity.UI_New.Main;
+using Il2CppAssets.Scripts.Unity.UI_New.Main.HeroSelect;
 using Il2CppAssets.Scripts.Unity.UI_New.Pause;
 using Il2CppAssets.Scripts.Unity.UI_New.Popups;
 using Il2CppAssets.Scripts.Unity.UI_New.Store;
@@ -11,8 +11,8 @@ using Il2CppAssets.Scripts.Unity.UI_New.Upgrade;
 using Il2CppPlayEveryWare.EpicOnlineServices;
 using Il2CppSystem.Threading.Tasks;
 using MelonLoader;
+using UnityEngine;
 using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 [assembly: MelonInfo(typeof(Mod), "BTD6 EOS Bypasser", "1.0.2", "Baydock")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6-Epic")]
@@ -20,8 +20,7 @@ using Object = UnityEngine.Object;
 namespace BTD6EOSBypasser;
 
 [HarmonyPatch]
-public class Mod : MelonMod
-{
+public class Mod : MelonMod {
     private const string DisabledStoreMessage =
         "In order to get mods to run on Epic Games, I had to gut Epic Store functionality.";
 
@@ -31,16 +30,14 @@ public class Mod : MelonMod
 
     public static MelonLogger.Instance Logger { get; private set; }
 
-    public override void OnInitializeMelon()
-    {
+    public override void OnInitializeMelon() {
         Logger = LoggerInstance;
     }
 
     [HarmonyPatch(typeof(EOSHostManager), nameof(EOSHostManager.WaitTillReady))]
     [HarmonyPrefix]
     // Stops from stalling on step 1 because EOSSDK is missing
-    public static bool BypassWaitForEOS(out Task __result)
-    {
+    public static bool BypassWaitForEOS(out Task __result) {
         __result = Task.CompletedTask;
         return false;
     }
@@ -48,7 +45,7 @@ public class Mod : MelonMod
     [HarmonyPatch(typeof(EOSHostManager), nameof(EOSHostManager.Start))]
     [HarmonyPrefix]
     // Stops EOSHostManager from setting up
-    public static bool BypassEOSHostManagerStart()=> false;
+    public static bool BypassEOSHostManagerStart() => false;
 
     [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.Awake))]
     [HarmonyPrefix]
@@ -63,8 +60,7 @@ public class Mod : MelonMod
     [HarmonyPatch(typeof(Modding), nameof(Modding.CheckForMods))]
     [HarmonyPrefix]
     // Idk why EOS is used here
-    public static bool BypassModCheck(out bool __result)
-    {
+    public static bool BypassModCheck(out bool __result) {
         // you silly, why wouldn't I return that you are in fact modding
         __result = true;
         return false;
@@ -73,8 +69,7 @@ public class Mod : MelonMod
     [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.Start))]
     [HarmonyPostfix]
     // Some cosmetic changes to better show that the store is unfunctional
-    public static void DisableStoreButton(MainMenu __instance)
-    {
+    public static void DisableStoreButton(MainMenu __instance) {
         var storeButton = __instance.storeBtn;
 
         // Make image look disabled to indicate it is not functional
@@ -83,15 +78,19 @@ public class Mod : MelonMod
         // Remove previous functionality, and add a message to explain why
         MainMenu.__c.__9__21_10 = null; // Where previous UnityAction is stored
         storeButton.onClick.RemoveAllListeners();
-        storeButton.onClick.AddListener(new Action(() =>
+        storeButton.onClick.AddListener(new System.Action(() =>
             PopupScreen.instance.ShowOkPopup(DisabledStoreMessage)));
     }
+
+    [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.Open))]
+    [HarmonyPrefix]
+    // I got an error here with a fresh account, idk, patching adds error catching so it's fine now
+    public static void BypassError() { }
 
     [HarmonyPatch(typeof(MainAccountPopup), nameof(MainAccountPopup.Awake))]
     [HarmonyPostfix]
     // Stop the user from trying to log in with epic since EOS is unfunctional
-    public static void DisableEpicLogin(MainAccountPopup __instance)
-    {
+    public static void DisableEpicLogin(MainAccountPopup __instance) {
         var loginEpicButton = __instance.loginEpicBtn;
 
         // Make image and text look disabled to indicate it is not functional
@@ -101,89 +100,49 @@ public class Mod : MelonMod
 
         // Remove previous functionality, and add a message to explain why
         loginEpicButton.onClick.RemoveAllListeners();
-        loginEpicButton.onClick.AddListener(new Action(() =>
+        loginEpicButton.onClick.AddListener(new System.Action(() =>
             PopupScreen.instance.ShowOkPopup(DisabledEpicLoginMessage)));
     }
 
     [HarmonyPatch(typeof(PauseScreen), nameof(PauseScreen.Open))]
     [HarmonyPostfix]
     // Some cosmetic changes to better show that the store is unfunctional
-    public static void DisablePauseStore(PauseScreen __instance)
-    {
-        // Make image look disabled to indicate it is not functional
-        __instance.storeButton.gameObject.GetComponent<Image>().color = __instance.storeButton.colors.disabledColor;
-
-        __instance.storeButton.onClick.RemoveAllListeners();
-        __instance.storeButton.onClick.AddListener(new Action(() =>
-            PopupScreen.instance.ShowOkPopup(DisabledStoreMessage)));
+    public static void DisablePauseStore(PauseScreen __instance) {
+        // Disable store button
+        __instance.storeButton.gameObject.SetActive(false);
     }
-
 
     [HarmonyPatch(typeof(UpgradeScreen), nameof(UpgradeScreen.Open))]
     [HarmonyPostfix]
     // Some cosmetic changes to better show that the store is unfunctional
-    public static void DisableUpgradePurchases(UpgradeScreen __instance)
-    {
-        // Make image look disabled to indicate it is not functional
-        __instance.purchaseAllTowerUpgradesIncludingParagon.transform.FindChild("Image").GetComponent<Image>().color =
-            __instance.purchaseAllTowerUpgradesIncludingParagon.button.colors.disabledColor;
-        __instance.purchaseAllTowerUpgradesIncludingParagon.icon.color =
-            __instance.purchaseAllTowerUpgradesIncludingParagon.button.colors.disabledColor;
-        __instance.purchaseAllTowerUpgradesIncludingParagon.transform.FindChild("Text")
-                .GetComponent<NK_TextMeshProUGUI>().color =
-            __instance.purchaseAllTowerUpgradesIncludingParagon.button.colors.disabledColor;
-        __instance.purchaseAllTowerUpgradesIncludingParagon.transform.FindChild("caveat")
-                .GetComponent<NK_TextMeshProUGUI>().color =
-            __instance.purchaseAllTowerUpgradesIncludingParagon.button.colors.disabledColor;
-
-        __instance.purchaseAllTowerUpgradesIncludingParagon.button.onClick.RemoveAllListeners();
-        __instance.purchaseAllTowerUpgradesIncludingParagon.button.onClick.AddListener(new Action(() =>
-            PopupScreen.instance.ShowOkPopup(DisabledStoreMessage)));
+    public static void DisableUpgradePurchases(UpgradeScreen __instance) {
+        // Destroy purchasing button
+        Object.Destroy(__instance.purchaseAllTowerUpgradesIncludingParagon.gameObject);
 
         //oh come on, another one
-        __instance.purchaseAllTowerUpgrades.transform.FindChild("Image").GetComponent<Image>().color =
-            __instance.purchaseAllTowerUpgrades.button.colors.disabledColor;
-        __instance.purchaseAllTowerUpgrades.icon.color =
-            __instance.purchaseAllTowerUpgrades.button.colors.disabledColor;
-        __instance.purchaseAllTowerUpgrades.transform.FindChild("Text").GetComponent<NK_TextMeshProUGUI>().color =
-            __instance.purchaseAllTowerUpgrades.button.colors.disabledColor;
-        __instance.purchaseAllTowerUpgrades.transform.FindChild("caveat").GetComponent<NK_TextMeshProUGUI>().color =
-            __instance.purchaseAllTowerUpgrades.button.colors.disabledColor;
-
-
-        __instance.purchaseAllTowerUpgrades.button.onClick.RemoveAllListeners();
-        __instance.purchaseAllTowerUpgrades.button.onClick.AddListener(new Action(() =>
-            PopupScreen.instance.ShowOkPopup(DisabledStoreMessage)));
+        Object.Destroy(__instance.purchaseAllTowerUpgrades.gameObject);
 
         //even more
-        __instance.purchaseParagonTowerUpgrades.transform.FindChild("Image").GetComponent<Image>().color =
-            __instance.purchaseParagonTowerUpgrades.button.colors.disabledColor;
-        __instance.purchaseParagonTowerUpgrades.icon.color =
-            __instance.purchaseParagonTowerUpgrades.button.colors.disabledColor;
-        __instance.purchaseParagonTowerUpgrades.transform.FindChild("Text").GetComponent<NK_TextMeshProUGUI>().color =
-            __instance.purchaseParagonTowerUpgrades.button.colors.disabledColor;
-        __instance.purchaseParagonTowerUpgrades.transform.FindChild("caveat").GetComponent<NK_TextMeshProUGUI>().color =
-            __instance.purchaseParagonTowerUpgrades.button.colors.disabledColor;
-
-
-        __instance.purchaseParagonTowerUpgrades.button.onClick.RemoveAllListeners();
-        __instance.purchaseParagonTowerUpgrades.button.onClick.AddListener(new Action(() =>
-            PopupScreen.instance.ShowOkPopup(DisabledStoreMessage)));
-
+        Object.Destroy(__instance.purchaseParagonTowerUpgrades.gameObject);
 
         //lots of purchasing buttons
-        __instance.purchaseTowerXP.icon.color = __instance.purchaseTowerXP.button.colors.disabledColor;
-        
-        __instance.purchaseTowerXP.button.onClick.RemoveAllListeners();
-        __instance.purchaseTowerXP.button.onClick.AddListener(new Action(() =>
-            PopupScreen.instance.ShowOkPopup(DisabledStoreMessage)));    
+        Object.Destroy(__instance.purchaseTowerXP.gameObject);
     }
-    
+
+    [HarmonyPatch(typeof(UpgradeScreen), nameof(UpgradeScreen.SetIapButtons))]
+    [HarmonyPrefix]
+    // Disable upgrade iaps
+    public static bool DisableUpgradeIaps() => false;
+
     [HarmonyPatch(typeof(TowerProductButton), nameof(TowerProductButton.StartPurchase))]
     [HarmonyPrefix]
-    // Some cosmetic changes to better show that the store is unfunctional'
-    public static bool DisableTowerProductButton(TowerProductButton __instance)
-    {
-        return false;
+    // Disable purchasing
+    public static bool DisableTowerProductButton(TowerProductButton __instance) => false;
+
+    [HarmonyPatch(typeof(HeroUpgradeDetails), nameof(HeroUpgradeDetails.Awake))]
+    [HarmonyPostfix]
+    // Disable hero bundles
+    public static void RemoveHeroBundles(HeroUpgradeDetails __instance) {
+        Object.Destroy(__instance.heroBundleBtn.transform.parent.gameObject);
     }
 }
